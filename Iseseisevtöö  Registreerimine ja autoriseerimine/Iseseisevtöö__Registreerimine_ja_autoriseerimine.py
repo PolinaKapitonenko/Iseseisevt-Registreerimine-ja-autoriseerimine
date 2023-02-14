1e4128fc8517df551649e4701b88da04a9f714e1
PolinaKapitonenko <120027531+PolinaKapitonenko@users.noreply.github.com>
2023-02-14 14:10:09 +0200

    Update Iseseisevtöö__Registreerimine_ja_autoriseerimine.py

--- Iseseisevtöö  Registreerimine ja autoriseerimine/Iseseisevtöö__Registreerimine_ja_autoriseerimine.py
+++ Iseseisevtöö  Registreerimine ja autoriseerimine/Iseseisevtöö__Registreerimine_ja_autoriseerimine.py
@@ -1,1 +1,19 @@
+from math import*
+from random import*
+from module1  import*
 
+
+l=["Polina"]
+p=["12345"]
+while True:
+    v=int(input("1-Regisreerimine/n2-Aurotiseerimine/n3-Välja"))
+    if v==1:
+        l,p=Registereerimine(l,p)
+        pass
+    elif v==2:
+        Autoriseerimine()
+    elif v==3:
+        break
+    else:
+        print("tee õige valik")
+         
