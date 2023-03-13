12702a774542db2824f3fc5e490fe3d49ca9ce06
PolinaKapitonenko <120027531+PolinaKapitonenko@users.noreply.github.com>
2023-03-13 11:21:54 +0200

    Update Iseseisevtöö__Registreerimine_ja_autoriseerimine.py

--- Iseseisevtöö  Registreerimine ja autoriseerimine/Iseseisevtöö__Registreerimine_ja_autoriseerimine.py
+++ Iseseisevtöö  Registreerimine ja autoriseerimine/Iseseisevtöö__Registreerimine_ja_autoriseerimine.py
@@ -5,47 +5,26 @@ from module1  import*
 
 log=["User1", "User2"]
 parool=["s1mple", "juppi"]
-while True:
-    print(log)
-    print(parool)
-    a=str(input("Kas soovite sisse logida? ")) #yas/no
-    if a=="jah":
-        print("Logi sisse")
-        print("Sisestage oma sisselogimine ja parool")
-        log=input("Logi sisse: ")
-        pass_=input("Parool: ")
-        if log in log and pass_ in parool:
-            print("Olete edukalt sisse loginud ")
-
-        else:
-           print("Vale sisselogimine või parool")
-           print("")
-           break
-
-    elif a=="ei":
-         b=str(input("Kas soovite registreeruda? "))
-         b=="jah"
-         print("registreerimine")
-         log,parool=registreerimine(log,parool)
-    else:
-         b=="ei"
-         c=str(input("lõpeta töö? ")) #ainult yas
-         if c=="jah":
-            print("Lõpp") 
-            break
-
 
 while True:
-    print(log)
-    print(parool)
-    valik = input("Kas soovite sisse logida? (jah/ei): ")
-    if valik == "jah":
-        print("Logi sisse")
-        kasutaja = input("Sisestage oma sisselogimine: ")
-        parool_sisestatud = input("Sisestage oma parool: ")
-        if kasutaja in log and parool_sisestatud == parool[log.index(kasutaja)]:
-            print("Olete edukalt sisse loginud!")
-        else:
-            print("Vale sisselogimine või parool")
-            break
+    print("Valige:")
+    print("1. Registreerima")
+    print("2. Logi sisse")
+    print("3. Muudata login voi salasõna")
+    print("4. Unustasid salasone")
+    print("5. Logi välja")
+    valik = input("Sisesta number (1-5): ")
+    if valik == '1':
+        register()
+    elif valik == '2':
+        authorize()
+    elif valik == '3':
+        change()
+    elif valik == '4':
+        forgotpassword()
+    elif valik == '5':
+        logout()
+        exit()
+   
+
          
